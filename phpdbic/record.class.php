ee163d1c0dd091e0feaa0b5aec17ef09c33617f5
Alastair McGowan-Douglas <al@podcats.in>
2010-04-09 20:48:47 +0100

    add lazy loading, save() and __get

--- phpdbic/record.class.php
+++ phpdbic/record.class.php
@@ -8,21 +8,76 @@ class Record {
     private $td;
 
     public function __construct( $schema, $td, $id = NULL ) {
-        $self->td = $td;
+        $this->td = $td;
+        $this->schema = $schema;
 
-        if( !empty($id) ){
-            $q = sprintf("SELECT * FROM `%s` WHERE `%s` = ?", $td->table_name(), $td->pk_col());
-            $this->data = $schema->query($q, array($id), PDO::FETCH_ASSOC);
+        # TODO: the id col could be an array of PK cols, defined by $td.
+        $this->id = $id;
+    }
+
+    public function load() {
+        # TODO: check for all PKs, not just id, since it may not be called id.
+        # also, pk_col will in future return an array in some cases.
+        if( !empty($this->data['id']) ){
+            $q = sprintf("SELECT * FROM `%s` WHERE `%s` = ?", $this->td->table_name(), $this->td->pk_col());
+            $this->data = $this->schema->query($q, array($this->id), PDO::FETCH_ASSOC);
+            $this->data = $this->data[0];
         }
     }
 
-    public function get_value( $column ) {
-        assert( $this->td->has_column( $column ));
+    public function save() {
+        # Check we have the necessary data before proceeding.
+        foreach ($this->td->columns() as $col) {
+            $coldata = $this->td->column($col);
+            assert( $coldata['is_nullable'] || !empty($this->data[$col]) );
+        }
 
-        return $this->data[$column];
+        $query = "INSERT INTO `%s` (%s) VALUES (%s)";
+        $query_cols = array();
+        $query_vals = array();
+
+        foreach ($this->td->columns() as $col) {
+            $coldata = $this->td->column($col);
+            if ($coldata['is_ai']) continue;
+
+            $query_cols[] = "`$col`";
+            $query_vals[] = $this->data[$col];
+        }
+
+        # TODO: This will devolve into the query class eventually. 
+        $query = sprintf($query, 
+                         $this->td->table_name(), 
+                         join(",", $query_cols), 
+                         join(",", array_pad(array(), count($query_vals), "?")));
+        $db->query($query, $query_vals);
+
     }
 
     public function data() {
         return array_merge( $this->data );
+    }
+
+    public function __get( $prop ) {
+        return $this->get($prop);
+    }
+
+    public function get( $prop ) {
+        assert( $this->td->has_column( $prop ) );
+
+        # Dude we are so, like, lazy.
+        if( empty($this->data) ) $this->load();
+        if( ! array_key_exists( $prop, $this->data ) ) $this->load();
+
+        return $this->data[$prop];
+    }
+
+    public function __set( $prop, $val ) {
+        $this->set($prop, $val);
+    }
+
+    public function set( $prop, $val ) {
+        assert( $this->td->has_column($prop) );
+
+        $this->data[$prop] = $val;
     }
 }
